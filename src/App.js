81e27dfa35c8d89d5d4c4eae4328902069413c74
fahim-irshad <66278214+fahim-irshad@users.noreply.github.com>
2020-06-06 00:52:33 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
      <h2>hello world</h2>
     </div>
+    <div id="container"></div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -10,7 +10,38 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+const { Map: LeafletMap, TileLayer, Marker, Popup } = ReactLeaflet
 
+class SimpleExample extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13
+    }
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <LeafletMap center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
+}
+
+
+ReactDOM.render(<SimpleExample />, document.getElementById('container'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
